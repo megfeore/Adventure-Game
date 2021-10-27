e2212d247ff3432b84793aa0448c67c16b6c141d
P4 <P4@DESKTOP-T3CRRL2>
2021-10-27 14:29:09 -0400

    Add project files.

---
+++ Adventure Game/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Adventure_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.headingLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "option 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(21, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "option 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(242, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "option 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(242, 305);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(172, 47);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "option 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // headingLabel
+            // 
+            this.headingLabel.BackColor = System.Drawing.Color.Goldenrod;
+            this.headingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headingLabel.Location = new System.Drawing.Point(18, 20);
+            this.headingLabel.Name = "headingLabel";
+            this.headingLabel.Size = new System.Drawing.Size(756, 46);
+            this.headingLabel.TabIndex = 4;
+            this.headingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Khaki;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(18, 97);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(396, 106);
+            this.outputLabel.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(436, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(338, 255);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 376);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.headingLabel);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label headingLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Adventure Game/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Adventure_Game
+{
+    public partial class Form1 : Form
+    {
+        //tracks what page the story is on
+        int page = 1;
+        Random randGen = new Random();
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+            headingLabel.Text = "Welcome to your Hogwarts Adventure!";
+            outputLabel.Text = "You get sent your Hogwarts letter. " +
+                "Do you accept to go to the School Of Wizardy and Witchcraft?";
+            button1.Text = "Accept";
+            button2.Text = "Do not accept";
+            button3.Visible = false;
+            button4.Visible = false;
+            pictureBox1.BackgroundImage = Properties.Resources.Hogwarts_Letter;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //sends you to a different page 
+            if (page == 1) { page = 2; }
+            else if (page == 2) { page = 4; }
+            else if (page == 3) { page = 9; }
+            else if (page == 4) { page = 6; }
+            else if (page == 5) { page = 6; }
+            else if (page == 6) { page = 7; }
+            else if (page == 7) { page = 1; }
+            else if (page == 8) { page = 11; }
+            else if (page == 9) { page = 1; }
+            else if (page == 10) { page = 8; }
+            else if (page == 11) { page = 15; }
+            else if (page == 12) { page = 15; }
+            else if (page == 13) { page = 15; }
+            else if (page == 14) { page = 15; }
+            else if (page == 15) { page = 17; }
+            else if (page == 16) { page = 1; }
+            else if (page == 17) { page = 18; }
+            else if (page == 18) { page = 99; }
+            else if (page == 19) { page = 1; }
+
+            //display game components and text based on the correct page
+            Game();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //picks random number between 1 and 10
+            int number = randGen.Next(1, 11);
+            
+            //sends you to a different page 
+            if (page == 1) { page = 3; }
+            else if (page == 2) { page = 5; }
+            else if (page == 3) { page = 10; }
+            else if (page == 4) { page = 6; }
+            else if (page == 5) { page = 6; }
+            else if (page == 6) { page = 8; }
+            else if (page == 7) { page = 99; }
+            else if (page == 8) { page = 12; }
+            else if (page == 9) { page = 99; }
+            else if (page == 11) { page = 16; }
+            else if (page == 12) { page = 16; }
+            else if (page == 13) { page = 16; }
+            else if (page == 14) { page = 16; }
+            else if (page == 15) { page = 16; }
+            else if (page == 16) { page = 99; }
+            
+            else if (page == 17)
+            {
+               //random generator if you die or make it through
+                if (number > 2)
+                {
+                    page = 18;
+                } 
+                else
+                {
+                    page = 19;
+                }
+            }
+            else if (page == 19) { page = 99; }
+            
+            //display game components and text based on the correct page
+            Game();
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //display game components and text based on the correct page
+            Game();
+            if (page == 1) { page = 3; }
+            else if (page == 8) { page = 13; }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //display game components and text based on the correct page
+            Game();
+            if (page == 1) { page = 3; }
+            else if (page == 8) { page = 14; }
+        }
+
+        public void Game()
+        {
+            switch (page)
+            {
+                case 1:
+                    headingLabel.Text = "Welcome to your Hogwarts Adventure!";
+                    outputLabel.Text = "You get sent your Hogwarts letter. " +
+                        "Do you accept to go to the School Of Wizardy and Witchcraft?";
+                    button1.Text = "Accept";
+                    button2.Text = "Do not accept";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Hogwarts_Letter;
+                    break;
+                case 2:
+                    outputLabel.Text = "You made your way onto the Hogwarts Express. " +
+                        "Do you buy candy from the trolly or eat the food your mom packed for you?";
+                    button1.Text = "Buy candy";
+                    button2.Text = "Eat packed food";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Hogwarts_Express_1;
+                    break;
+                case 3:
+                    outputLabel.Text = "You did not accept the experience of a lifetime. " +
+                        "You are now punished and sent away to be a house maid for the Dursley's." +
+                        "Do you stay and take care of them? Or try to escape?";
+                    button1.Text = "Stay";
+                    button2.Text = "Escape";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Dursleys;
+                    break;
+                case 4:
+                    outputLabel.Text = "You decide to buy candy from the trolly... " +
+                        "do you buy signature chocolate frogs, or jelly beans?";
+                    button1.Text = "Frogs";
+                    button2.Text = "Jelly Beans";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Trolly;
+                    break;
+                case 5:
+                    outputLabel.Text = "You take a bite out of your sandwhich and you now regret not buying candy. " +
+                        "Do you force the sandwhich down your throat or throw it out the window?";
+                    button1.Text = "Keep eating";
+                    button2.Text = "Throw out window";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Sandwich;
+                    break;
+                case 6:
+                    outputLabel.Text = "You have now arrived at Hogwarts! " +
+                        "How do you make your way to the castle?";
+                    button1.Text = "Walk";
+                    button2.Text = "Take a boat";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Castle_2;
+                    break;
+                case 7:
+                    outputLabel.Text = "You decided to walk to the castle. " +
+                        "Unfortunately that was not so smart. " +
+                        "You walked into the Forbidden Forest and a giant ate you. " +
+                        "Would you like to play again?";
+                    button1.Text = "Yes";
+                    button2.Text = "No";
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Forbidden_Forest;
+                    break;
+                case 8:
+                    outputLabel.Text = "Yay you made it to the castle! " +
+                        "You are now in the Great Hall and being sorted into the Hogwart's Houses. " +
+                        "How would you describe yourself to the sorting hat?";
+                    button1.Text = "Brave";
+                    button2.Text = "Shy";
+                    button3.Visible = true;
+                    button4.Visible = true;
+                    button3.Text = "Wicked";
+                    button4.Text = "Smart";
+                    pictureBox1.BackgroundImage = Properties.Resources.Hermione_Granger_wearing_the_Sorting_Hat;
+                    break;
+                case 9:
+                    outputLabel.Text = "You decided to stay. That was a terrible choice. " +
+                        "The Dursley's got so annoyed with you that they locked you in the " +
+                        "cupboard under the stairs. You are never seen again. " +
+                        "Want to play again?";
+                    button1.Text = "Yes";
+                    button2.Text = "No";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Cupboard;
+                    break;
+                case 10:
+                    outputLabel.Text = "Your escape worked! Hagrid thankfully " +
+                        "comes to rescue you and takes you to Hogwarts! " +
+                        "Would you like to continue your adventure?";
+                    button1.Text = "Continue";
+                    button2.Visible = false;
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Hagr;
+                    break;
+                case 11:
+                    outputLabel.Text = "You are a brave soul! You got sorted into " +
+                        "Gryffindor House! Would you like to go to sleep or " +
+                        "celebrate by throwing a party?";
+                    button1.Text = "Sleep";
+                    button2.Text = "Party";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Gryffindor;
+                    break;
+                case 12:
+                    outputLabel.Text = "You are a quiet soul filled with kindness!" +
+                        "You got sorted into Hufflepuff House! " +
+                        "Would you like to go to sleep or " +
+                        "celebrate by throwing a party?";
+                    button1.Text = "Sleep";
+                    button2.Text = "Party";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Hufflepuff;
+                    break;
+                case 13:
+                    outputLabel.Text = "You are full of ideas and greatness! " +
+                        "You got sorted into Slytherin House! " +
+                        "Would you like to go to sleep or celebrate by throwing a party?";
+                    button1.Text = "Sleep";
+                    button2.Text = "Party";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Slytherin;
+                    break;
+                case 14:
+                    outputLabel.Text = "You have a mind of gold!" +
+                        "You got sorted into Ravenclaw House! " +
+                        "Would you like to go to sleep or celebrate by throwing a party?";
+                    button1.Text = "Sleep";
+                    button2.Text = "Party";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.R;
+                    break;
+                case 15:
+                    outputLabel.Text = "You had such a wonderful sleep in your first night " +
+                        "in the castle. Now you have your first class. " +
+                        "However, you are quite tired. Do you go to your potions class? " +
+                        "Or fall back to sleep?";
+                    button1.Text = "Go to class";
+                    button2.Text = "Sleep";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Hagr;
+                    break;
+                case 16:
+                    outputLabel.Text = "Oh no! You got expelled. " +
+                        "See you never! Would you like to play again?";
+                    button1.Text = "Yes";
+                    button2.Text = "No";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Expelled;
+                    break;
+                   
+                case 17:
+                    outputLabel.Text = "Going to class was a smart choice! " +
+                        "You are making a love potion in class today. " +
+                        "Do you want to follow the recipe, or no?";
+                    button1.Text = "Yes";
+                    button2.Text = "No";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Pot;
+                    break;
+                case 18:
+                    outputLabel.Text = "Congratulations! You have made it through your " +
+                        "first class! Your future at Hogwarts looks great!";
+                    button1.Text = "End Game";
+                    button2.Visible = false;
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Castle_Daytime;
+                    break;
+                case 19:
+                    outputLabel.Text = "Wow you have terrible luck! " +
+                        "The potion exploded in your face and tragically you passed away. " +
+                        "Want to play again?";
+                    button1.Text = "Yes";
+                    button2.Text = "No";
+                    button3.Visible = false;
+                    button4.Visible = false;
+                    pictureBox1.BackgroundImage = Properties.Resources.Exploded;
+                    break;
+                case 99:
+                    headingLabel.Text = "Thank you for playing!";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    pictureBox1.BackgroundImage = Properties.Resources.Willow;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
+
